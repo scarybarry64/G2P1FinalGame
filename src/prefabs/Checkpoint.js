a2c4be965fea5e72b45873687d649155b7f00fa7
tmoropou <tmoropou@ucsc.edu>
2020-06-03 15:30:28 -0700

    Checkpoint optimization

--- src/prefabs/Checkpoint.js
+++ src/prefabs/Checkpoint.js
@@ -9,22 +9,30 @@ class Checkpoint{
         scene.add.existing(this);
     }
 
+    create() {
+        this.checkpointSet = false;
+    }
+    
+
     update(){
         //Check if the given player object makes contact with the checkpoint
-        if(this.player.x < this.x + 55 && this.player.x > this.x - 55) {
+        if(this.player.x < this.x + 25 && this.player.x > this.x - 25) {
             if(this.player.y < this.y + 20 && this.player.y > this.y - 20) {
-                console.log("player reached checkpoint");
-                this.tempx = localStorage.getItem('checkpointx');
-                this.tempy = localStorage.getItem('checkpointy');
-                console.log(typeof this.tempy);
-                //console.log("tempx = " + this.tempx);
+                if(!this.checkpointSet){
+                    // console.log("player reached checkpoint");
+                    this.tempx = localStorage.getItem('checkpointx');
+                    this.tempy = localStorage.getItem('checkpointy');
+                    // console.log(typeof this.tempy);
+                    //console.log("tempx = " + this.tempx);
+                    this.checkpointSet = true;
 
-                //Change the stored checkpoint x and y if there is no previous reached checkpoints
-                //or if the current checkpoint is the most advanced
-                if(this.y < this.tempy || this.tempy == null){
-                    localStorage.setItem('checkpointx', this.x);
-                    localStorage.setItem('checkpointy', this.y);
-                    console.log("changed checkpoint");
+                    //Change the stored checkpoint x and y if there is no previous reached checkpoints
+                    //or if the current checkpoint is the most advanced
+                    if(this.y < this.tempy || this.tempy == null){
+                        localStorage.setItem('checkpointx', this.x);
+                        localStorage.setItem('checkpointy', this.y);
+                        // console.log("changed checkpoint");
+                    }
                 }
             }
         }
